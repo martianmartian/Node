a9d33e4b6c54f7e957eae79834ceae1d99660430
iamwave007 <iamwave007@hotmail.com>
2014-10-18 18:50:22 -0400

    final version of node beginner

--- server.js
+++ server.js
@@ -2,28 +2,12 @@ var http=require("http");
 var url=require("url");
 
 function start (route,handle) {
-	function onRequest(request,response){
-        var postData="";
-		var pathname=url.parse(request.url).pathname;
-		console.log("pathname  "+pathname + " is received.");
+	function onRequest(request,response) {
 
-        request.setEncoding("utf8");
-
-
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received POST data chunk '"+
-            postDataChunk + "'.");
-        });
-
-        request.addListener("end",function(){
-            route(handle,pathname,response,postData);
-        });
-
-		//instead of having the "response" here in the server.js
-		//passing it down 
-
-	}
+        var pathname = url.parse(request.url).pathname;
+        console.log("pathname  " + pathname + " is received.");
+        route(handle, pathname, response, request);
+    }
 
 	http.createServer(onRequest).listen(8888);
 	//console.log("Server has started.");
