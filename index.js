822f42b3df5e7139e2c1e9a8e8f85101e600e47a
iamwave007 <iamwave007@hotmail.com>
2014-10-18 12:39:40 -0400

    before non-blocking

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var server = require("./server");
-var router = require('./router');
-var requestHandlers = require('./requestHandlers');
+var server=require("./server");
+var router=require("./router");
+var requestHandlers=require("./requestHandlers");
 
-var handle = {};
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
-handle["/show"] = requestHandlers.show;
 
-server.start(router.route, handle);
+var handle={};
+handle["/"]=requestHandlers.start;
+handle["/start"]=requestHandlers.start;
+handle["/upload"]=requestHandlers.upload;
+
+
+server.start(router.route,handle);
--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-var http = require('http');
-var url = require('url');
+var http=require("http");
+var url=require("url");
 
-function start(route, handle) {
-  function onRequest(request, response) {
-    var postData = "";
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+function start (route,handle) {
+	function onRequest(require,response){
+		var pathname=url.parse(request.url).pathname;
+		console.log("pathname  "+pathname + " is received.");
 
-    request.setEncoding("utf8");
+		route(handle,pathname);
 
-    request.addListener("data", function(postDataChunk) {
-      postData += postDataChunk;
-      console.log("Received POST data chunk '" + postDataChunk + "'.");
-    });
+		response.writeHead(200,{"Content-Type":"text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
 
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);
-    });
-  }
-
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+	http.createServer(onRequest).listen(8888);
+	//console.log("Server has started.");
 }
-
-exports.start = start;
